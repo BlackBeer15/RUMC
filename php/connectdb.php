1fb41ef3e2ae218db4881d7e349a14182c4d099f
BlackBeer15 <little4biz@gmail.com>
2025-04-14 18:57:19 +0700

    Clear auth

--- php/connectdb.php
+++ php/connectdb.php
@@ -1,6 +1,6 @@
 <?php 
 	try {
-		$conn = new PDO('mysql:host=localhost;dbname=rumc_spo','Dmitry','Soroka-456585');
+		$conn = new PDO('mysql:host=localhost;dbname=rumc_spo','#','#');
 	} catch (PDOException $pe) {
 		die("Could not connect to the database $dbname :" . $pe->getMessage());
 	}
